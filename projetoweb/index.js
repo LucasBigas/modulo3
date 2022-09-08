d203c4672e759070a94c42d038a18b023d8c8129
LUCAS BIGAS PADILHA <lucaspadilha@univille.br>
2022-09-08 17:24:06 -0300

    Projetoweb

--- projetoweb/index.js
+++ projetoweb/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars');
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', engine());
 
 app.get("/", function(req,res){
-    res.send(`<h1>EU NAO ACREDITO ${req.query['nome']}</h1>`);
+    res.send(`<h1>EU NAO ACREDITO ${req.query['nome']} ${req.query['sobrenome']}</h1>`);
 })
 
-app.listen(3000, () => {
-    console.log("Srvidor online")
+app.listen(3000, () =>{ //CALLBACK
+    console.log("Servidor online");
     console.log("http://localhost:3000/");
-
 });
